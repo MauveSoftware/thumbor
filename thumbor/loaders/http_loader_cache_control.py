89669b028fa1080d0290a5cfb8ee03b0e2b7edc6
Daniel Czerwonk <daniel@dan-nrw.de>
2021-07-01 16:44:39 +0200

    add loader and storage implentation hornoring caching headers

---
+++ thumbor/loaders/http_loader_cache_control.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# thumbor imaging service
+# https://github.com/thumbor/thumbor/wiki
+
+# Licensed under the MIT license:
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import re
+
+from thumbor.loaders import http_loader
+
+def _return_contents(response, url, context, req_start=None):
+    result = http_loader.return_contents(response, url, context, req_start)
+
+    if result.metadata is None:
+        return result
+
+    cache_control = result.metadata.get("Cache-Control")
+    if cache_control is not None:
+        _update_max_age(context, cache_control)
+    else:
+        context.request.max_age = 0
+
+    return result
+
+def _update_max_age(context, cache_control):
+    match = re.search("max-age\\s*=\\s*(\\d+)", cache_control)
+    if match:
+        context.request.max_age = int(match[1])
+        return
+
+    context.request.max_age = 0
+
+async def load(context, url):
+    return await http_loader.load(context, url, return_contents_fn=_return_contents,)
---
+++ thumbor/storages/file_storage_cache_control.py
@@ -0,0 +1,169 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# thumbor imaging service
+# https://github.com/thumbor/thumbor/wiki
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/mit-license
+# Copyright (c) 2011 globo.com thumbor@googlegroups.com
+
+import hashlib
+import os
+from datetime import datetime
+from json import dumps, loads
+from os.path import dirname, exists, getmtime, splitext
+from shutil import move
+from uuid import uuid4
+
+import thumbor.storages as storages
+from thumbor.utils import logger
+
+
+class Storage(storages.BaseStorage):
+    EXPIRE_EXT = ".max_age"
+
+    async def put(self, path, file_bytes):
+        if self.context.request.max_age is not None and self.context.request.max_age == 0:
+            return
+
+        file_abspath = self.path_on_filesystem(path)
+        temp_abspath = "%s.%s" % (file_abspath, str(uuid4()).replace("-", ""))
+        file_dir_abspath = dirname(file_abspath)
+
+        logger.debug("creating tempfile for %s in %s...", path, temp_abspath)
+
+        self.ensure_dir(file_dir_abspath)
+
+        with open(temp_abspath, "wb") as _file:
+            _file.write(file_bytes)
+
+        if self.context.request.max_age is not None:
+            with open(temp_abspath + Storage.EXPIRE_EXT, "wb") as _file:
+                _file.write(str.encode(str(self.context.request.max_age)))
+                move(temp_abspath + Storage.EXPIRE_EXT, file_abspath + Storage.EXPIRE_EXT)
+
+        logger.debug("moving tempfile %s to %s...", temp_abspath, file_abspath)
+        move(temp_abspath, file_abspath)
+
+        return path
+
+    async def put_crypto(self, path):
+        if not self.context.config.STORES_CRYPTO_KEY_FOR_EACH_IMAGE:
+            return
+
+        file_abspath = self.path_on_filesystem(path)
+        file_dir_abspath = dirname(file_abspath)
+
+        self.ensure_dir(file_dir_abspath)
+
+        if not self.context.server.security_key:
+            raise RuntimeError(
+                "STORES_CRYPTO_KEY_FOR_EACH_IMAGE can't be "
+                "True if no SECURITY_KEY specified"
+            )
+
+        crypto_path = "%s.txt" % splitext(file_abspath)[0]
+        temp_abspath = "%s.%s" % (crypto_path, str(uuid4()).replace("-", ""))
+        with open(temp_abspath, "wb") as _file:
+            _file.write(self.context.server.security_key.encode())
+
+        move(temp_abspath, crypto_path)
+        logger.debug(
+            "Stored crypto at %s (security key: %s)",
+            crypto_path,
+            self.context.server.security_key,
+        )
+
+        return file_abspath
+
+    async def put_detector_data(self, path, data):
+        file_abspath = self.path_on_filesystem(path)
+
+        path = "%s.detectors.txt" % splitext(file_abspath)[0]
+        temp_abspath = "%s.%s" % (path, str(uuid4()).replace("-", ""))
+
+        file_dir_abspath = dirname(file_abspath)
+        self.ensure_dir(file_dir_abspath)
+
+        with open(temp_abspath, "w") as _file:
+            _file.write(dumps(data))
+
+        move(temp_abspath, path)
+
+        return file_abspath
+
+    async def get(self, path):
+        abs_path = self.path_on_filesystem(path)
+
+        resource_available = await self.exists(path, path_on_filesystem=abs_path)
+        if not resource_available:
+            return None
+
+        expire_time = self.get_expire_time(path)
+        self.context.request.max_time = expire_time
+
+        with open(self.path_on_filesystem(path), "rb") as source_file:
+            return source_file.read()
+
+    async def get_crypto(self, path):
+        file_abspath = self.path_on_filesystem(path)
+        crypto_file = "%s.txt" % (splitext(file_abspath)[0])
+
+        if not exists(crypto_file):
+            return None
+
+        with open(crypto_file, "r") as crypto_f:
+            return crypto_f.read()
+
+    async def get_detector_data(self, path):
+        file_abspath = self.path_on_filesystem(path)
+        path = "%s.detectors.txt" % splitext(file_abspath)[0]
+
+        resource_available = await self.exists(path, path_on_filesystem=path)
+
+        if not resource_available:
+            return None
+
+        return loads(open(path, "r").read())
+
+    def path_on_filesystem(self, path):
+        digest = hashlib.sha1(path.encode("utf-8")).hexdigest()
+        return "%s/%s/%s" % (
+            self.context.config.FILE_STORAGE_ROOT_PATH.rstrip("/"),
+            digest[:2],
+            digest[2:],
+        )
+
+    async def exists(self, path, path_on_filesystem=None):  # pylint: disable=arguments-differ
+        if path_on_filesystem is None:
+            path_on_filesystem = self.path_on_filesystem(path)
+
+        expire_time = self.get_expire_time(path_on_filesystem)
+        return os.path.exists(path_on_filesystem) and not self.__is_expired(path_on_filesystem, expire_time)
+
+    async def remove(self, path):
+        n_path = self.path_on_filesystem(path)
+
+        expire_file = n_path + Storage.EXPIRE_EXT
+        if exists(expire_file):
+            os.remove(expire_file)
+
+        return os.remove(n_path)
+
+    def __is_expired(self, path, expire_in_seconds):
+        if expire_in_seconds is None or expire_in_seconds == 0:
+            return False
+
+        timediff = datetime.now() - datetime.fromtimestamp(getmtime(path))
+        return timediff.total_seconds() > expire_in_seconds
+
+    def get_expire_time(self, path):
+        file_abspath = path + Storage.EXPIRE_EXT
+
+        if not exists(file_abspath):
+            return self.context.config.get("STORAGE_EXPIRATION_SECONDS", None)
+
+        with open(file_abspath, "rb") as expire_file:
+            buffer = expire_file.read()
+            return int(buffer.decode())
