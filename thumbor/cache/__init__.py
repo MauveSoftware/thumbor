9fc7f5339d02602bd4296f2b43808c5418854f0a
Daniel Czerwonk <czerwonk@mauve.de>
2023-04-10 18:35:23 +0200

    add new module encapsulating file cache functionality

---
+++ thumbor/cache/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# thumbor imaging service
+# https://github.com/thumbor/thumbor/wiki
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/mit-license
+# Copyright (c) 2023 Mauve Mailorder Software 
+
