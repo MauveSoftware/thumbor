fcf5ba6480875c9355c3111de6c3f49eb89a5a61
Daniel Czerwonk <czerwonk@mauve.de>
2023-04-10 18:32:38 +0200

    start refactoring of file cache

--- thumbor/result_storages/file_storage_cache_control.py
+++ thumbor/result_storages/file_storage_cache_control.py
@@ -10,20 +10,25 @@
 
 import hashlib
 from datetime import datetime
+from os import symlink, remove
 from os.path import abspath, dirname, exists, getmtime, isdir, isfile, join
-from shutil import move
 from urllib.parse import unquote
-from uuid import uuid4
 
 import pytz
 
 from thumbor.engines import BaseEngine
 from thumbor.result_storages import BaseStorage, ResultStorageResult
 from thumbor.utils import deprecated, logger
+from thumbor.cache.file_cache import FileCache 
 
 class Storage(BaseStorage):
     PATH_FORMAT_VERSION = "v2"
-    EXPIRE_EXT = ".max_age"
+
+    def __init__(self, context):
+        super().__init__(context)
+        self.cache = FileCache("RESULT_STORAGES",
+                               self.context.config.RESULT_STORAGE_FILE_STORAGE_ROOT_PATH.rstrip("/"),
+                               0)
 
     @property
     def is_auto_webp(self):
@@ -36,36 +41,10 @@ class Storage(BaseStorage):
         if self.context.request.max_age_shared is None and self.context.request.max_age is not None and self.context.request.max_age == 0:
             return
 
-        file_abspath = self.normalize_path(self.context.request.url)
-        if not self.validate_path(file_abspath):
-            logger.warning(
-                "[RESULT_STORAGE] unable to write outside root path: %s", file_abspath
-            )
-            return
-
-        temp_abspath = "%s.%s" % (file_abspath, str(uuid4()).replace("-", ""))
-        file_dir_abspath = dirname(file_abspath)
-        logger.debug(
-            "[RESULT_STORAGE] putting at %s (%s)", file_abspath, file_dir_abspath
-        )
-
-        self.ensure_dir(file_dir_abspath)
-
-        with open(temp_abspath, "wb") as _file:
-            _file.write(image_bytes)
-
-        if self.context.request.max_age is not None:
-            with open(temp_abspath + Storage.EXPIRE_EXT, "wb") as _file:
-                self.write_expire_file(_file)
-            move(temp_abspath + Storage.EXPIRE_EXT, file_abspath + Storage.EXPIRE_EXT)
-
-        move(temp_abspath, file_abspath)
-
-    def write_expire_file(self, _file):
-        _file.write(str.encode(str(self.context.request.max_age)))
-        
-        if self.context.request.max_age_shared is not None:
-            _file.write(str.encode("," + str(self.context.request.max_age_shared)))
+        symlink_abspath = self.normalize_path(self.context.request.url)
+        self.cache.put(symlink_abspath, 
+                       image_bytes, 
+                       self.context.request.max_age, self.context.request.max_age_shared)
 
     async def get(self):
         if self.context.request.bypass_cache:
@@ -74,67 +53,23 @@ class Storage(BaseStorage):
 
         path = self.context.request.url
         file_abspath = self.normalize_path(path)
-
-        if not self.validate_path(file_abspath):
-            logger.warning(
-                "[RESULT_STORAGE] unable to read from outside root path: %s",
-                file_abspath,
-            )
+        cache_res = self.cache.get(file_abspath)
+        if not cache_res.found:
             return None
 
-        logger.debug("[RESULT_STORAGE] getting from %s", file_abspath)
+        if cache_res.max_age is not None:
+            self.context.request.max_age = cache_res.max_age
+            self.context.request.max_age_shared = cache_res.max_age_shared
 
-        if isdir(file_abspath):
-            logger.warning(
-                "[RESULT_STORAGE] cache location is a directory: %s", file_abspath
-            )
-            return None
-
-        if not exists(file_abspath):
-            legacy_path = self.normalize_path_legacy(path)
-            if isfile(legacy_path):
-                logger.debug(
-                    "[RESULT_STORAGE] migrating image from old location at %s",
-                    legacy_path,
-                )
-                self.ensure_dir(dirname(file_abspath))
-                move(legacy_path, file_abspath)
-            else:
-                logger.debug("[RESULT_STORAGE] image not found at %s", file_abspath)
-                return None
-
-        max_age, max_age_shared = self.get_expire_time(file_abspath)
-        expire_time = max_age
-        if max_age_shared is not None:
-            expire_time = max_age_shared
-
-        if self.is_expired(file_abspath, expire_time):
-            logger.debug("[RESULT_STORAGE] cached image has expired")
-            return None
-
-        with open(file_abspath, "rb") as image_file:
-            buffer = image_file.read()
-
-        if max_age is not None:
-            self.context.request.max_age = max_age
-            self.context.request.max_age_shared = max_age_shared
-
-        result = ResultStorageResult(
-            buffer=buffer,
+        return ResultStorageResult(
+            buffer=cache_res.data,
             metadata={
                 "LastModified": datetime.fromtimestamp(getmtime(file_abspath)).replace(
                     tzinfo=pytz.utc
                 ),
-                "ContentLength": len(buffer),
-                "ContentType": BaseEngine.get_mimetype(buffer),
+                "ContentLength": len(cache_res.data),
+                "ContentType": BaseEngine.get_mimetype(cache_res.data),
             },
-        )
-
-        return result
-
-    def validate_path(self, path):
-        return abspath(path).startswith(
-            self.context.config.RESULT_STORAGE_FILE_STORAGE_ROOT_PATH
         )
 
     def normalize_path(self, path):
@@ -148,64 +83,13 @@ class Storage(BaseStorage):
             digest[4:],
         )
 
-    def normalize_path_legacy(self, path):
-        path = unquote(path)
-        path_segments = [
-            self.context.config.RESULT_STORAGE_FILE_STORAGE_ROOT_PATH.rstrip("/"),
-            Storage.PATH_FORMAT_VERSION,
-        ]
-        if self.is_auto_webp:
-            path_segments.append("webp")
-
-        path_segments.extend([self.partition(path), path.lstrip("/")])
-
-        normalized_path = join(*path_segments).replace("http://", "")
-        return normalized_path
-
-    def partition(self, path_raw):
-        path = path_raw.lstrip("/")
-        return join("".join(path[0:2]), "".join(path[2:4]))
-
-    def is_expired(self, path, expire_in_seconds):
-        if expire_in_seconds is None or expire_in_seconds == 0:
-            return False
-
-        timediff = datetime.now() - datetime.fromtimestamp(getmtime(path))
-        return timediff.total_seconds() > expire_in_seconds
-
-    def get_expire_time(self, path):
-        file_abspath = path + Storage.EXPIRE_EXT
-
-        if not exists(file_abspath):
-            return self.context.config.get("RESULT_STORAGE_EXPIRATION_SECONDS", None), None
-
-        with open(file_abspath, "rb") as expire_file:
-            buffer = expire_file.read()
-            max_ages = buffer.decode().split(",")
-            if len(max_ages) == 2:
-                return int(max_ages[0]), int(max_ages[1])
-
-            return int(max_ages[0]), None
-
     @deprecated("Use result's last_modified instead")
     def last_updated(self):
         path = self.context.request.url
         file_abspath = self.normalize_path(path)
-        if not self.validate_path(file_abspath):
-            logger.warning(
-                "[RESULT_STORAGE] unable to read from outside root path: %s",
-                file_abspath,
-            )
-            return True
-        logger.debug("[RESULT_STORAGE] getting from %s", file_abspath)
 
-        if not exists(file_abspath):
-            logger.debug("[RESULT_STORAGE] image not found at %s", file_abspath)
-            return True
-
-        expire_time = self.get_expire_time(file_abspath)
-        if self.is_expired(file_abspath, expire_time):
-            logger.debug("[RESULT_STORAGE] image not found at %s", file_abspath)
+        if not self.cache.exists(file_abspath):
+            logger.debug("[RESULT_STORAGE] image not found or expired at %s", file_abspath)
             return True
 
         return datetime.fromtimestamp(getmtime(file_abspath)).replace(tzinfo=pytz.utc)
--- thumbor/storages/file_storage_cache_control.py
+++ thumbor/storages/file_storage_cache_control.py
@@ -8,20 +8,20 @@
 # http://www.opensource.org/licenses/mit-license
 # Copyright (c) 2011 globo.com thumbor@googlegroups.com
 
+from functools import cache
 import hashlib
-import os
-from datetime import datetime
-from json import dumps, loads
-from os.path import dirname, exists, getmtime, splitext
-from shutil import move
-from uuid import uuid4
+from thumbor.cache.file_cache import FileCache
 
 import thumbor.storages as storages
 from thumbor.utils import logger
 
 
 class Storage(storages.BaseStorage):
-    EXPIRE_EXT = ".max_age"
+    def __init__(self, context):
+        super().__init__(context)
+        self.cache = FileCache("STORAGE", 
+                               self.context.config.FILE_STORAGE_ROOT_PATH.rstrip("/"),
+                               self.context.config.get("STORAGE_EXPIRATION_SECONDS", None))
 
     async def put(self, path, file_bytes):
         if self.context.request.max_age_shared is not None and self.context.request.max_age_shared == 0:
@@ -31,76 +31,10 @@ class Storage(storages.BaseStorage):
             return
 
         file_abspath = self.path_on_filesystem(path)
-        temp_abspath = "%s.%s" % (file_abspath, str(uuid4()).replace("-", ""))
-        file_dir_abspath = dirname(file_abspath)
-
-        logger.debug("creating tempfile for %s in %s...", path, temp_abspath)
-
-        self.ensure_dir(file_dir_abspath)
-
-        with open(temp_abspath, "wb") as _file:
-            _file.write(file_bytes)
-
-        if self.context.request.max_age is not None:
-            with open(temp_abspath + Storage.EXPIRE_EXT, "wb") as _file:
-                self.write_expire_file(_file)
-            move(temp_abspath + Storage.EXPIRE_EXT, file_abspath + Storage.EXPIRE_EXT)
-
-        logger.debug("moving tempfile %s to %s...", temp_abspath, file_abspath)
-        move(temp_abspath, file_abspath)
-
-        return path
-
-    def write_expire_file(self, _file):
-        _file.write(str.encode(str(self.context.request.max_age)))
-        
-        if self.context.request.max_age_shared is not None:
-            _file.write(str.encode("," + str(self.context.request.max_age_shared)))
-
-    async def put_crypto(self, path):
-        if not self.context.config.STORES_CRYPTO_KEY_FOR_EACH_IMAGE:
-            return
-
-        file_abspath = self.path_on_filesystem(path)
-        file_dir_abspath = dirname(file_abspath)
-
-        self.ensure_dir(file_dir_abspath)
-
-        if not self.context.server.security_key:
-            raise RuntimeError(
-                "STORES_CRYPTO_KEY_FOR_EACH_IMAGE can't be "
-                "True if no SECURITY_KEY specified"
-            )
-
-        crypto_path = "%s.txt" % splitext(file_abspath)[0]
-        temp_abspath = "%s.%s" % (crypto_path, str(uuid4()).replace("-", ""))
-        with open(temp_abspath, "wb") as _file:
-            _file.write(self.context.server.security_key.encode())
-
-        move(temp_abspath, crypto_path)
-        logger.debug(
-            "Stored crypto at %s (security key: %s)",
-            crypto_path,
-            self.context.server.security_key,
-        )
-
-        return file_abspath
-
-    async def put_detector_data(self, path, data):
-        file_abspath = self.path_on_filesystem(path)
-
-        path = "%s.detectors.txt" % splitext(file_abspath)[0]
-        temp_abspath = "%s.%s" % (path, str(uuid4()).replace("-", ""))
-
-        file_dir_abspath = dirname(file_abspath)
-        self.ensure_dir(file_dir_abspath)
-
-        with open(temp_abspath, "w") as _file:
-            _file.write(dumps(data))
-
-        move(temp_abspath, path)
-
-        return file_abspath
+        self.cache.put(file_abspath, 
+                       file_bytes, 
+                       self.context.request.max_age, 
+                       self.context.request.max_age_shared)
 
     async def get(self, path):
         if self.context.request.bypass_cache:
@@ -108,42 +42,14 @@ class Storage(storages.BaseStorage):
             return None
 
         abs_path = self.path_on_filesystem(path)
-
-        resource_available = await self.exists(path, path_on_filesystem=abs_path)
-        if not resource_available:
+        cache_res = self.cache.get(abs_path)
+        if not cache_res.found:
             return None
 
-        max_age, max_age_shared = self.get_expire_time(abs_path)
-        if max_age is not None:
-            self.context.request.max_age = max_age
-            self.context.request.max_age_shared = max_age_shared
+        return cache_res.data
 
-        with open(self.path_on_filesystem(path), "rb") as source_file:
-            return source_file.read()
-
-    async def get_crypto(self, path):
-        file_abspath = self.path_on_filesystem(path)
-        crypto_file = "%s.txt" % (splitext(file_abspath)[0])
-
-        if not exists(crypto_file):
-            return None
-
-        with open(crypto_file, "r") as crypto_f:
-            return crypto_f.read()
-
-    async def get_detector_data(self, path):
-        file_abspath = self.path_on_filesystem(path)
-        path = "%s.detectors.txt" % splitext(file_abspath)[0]
-
-        resource_available = await self.exists(path, path_on_filesystem=path)
-
-        if not resource_available:
-            return None
-
-        return loads(open(path, "r").read())
-
-    def path_on_filesystem(self, path):
-        digest = hashlib.sha1(path.encode("utf-8")).hexdigest()
+    def path_on_filesystem(self, hash_data):
+        digest = hashlib.sha1(hash_data.encode("utf-8")).hexdigest()
         return "%s/%s/%s" % (
             self.context.config.FILE_STORAGE_ROOT_PATH.rstrip("/"),
             digest[:2],
@@ -154,39 +60,8 @@ class Storage(storages.BaseStorage):
         if path_on_filesystem is None:
             path_on_filesystem = self.path_on_filesystem(path)
 
-        max_age, max_age_shared = self.get_expire_time(path_on_filesystem)
-        expire_time = max_age
-        if max_age_shared is not None:
-            expire_time = max_age_shared
-
-        return os.path.exists(path_on_filesystem) and not self.__is_expired(path_on_filesystem, expire_time)
+        return self.cache.exists(path_on_filesystem)
 
     async def remove(self, path):
         n_path = self.path_on_filesystem(path)
-
-        expire_file = n_path + Storage.EXPIRE_EXT
-        if exists(expire_file):
-            os.remove(expire_file)
-
-        return os.remove(n_path)
-
-    def __is_expired(self, path, expire_in_seconds):
-        if expire_in_seconds is None or expire_in_seconds == 0:
-            return False
-
-        timediff = datetime.now() - datetime.fromtimestamp(getmtime(path))
-        return timediff.total_seconds() > expire_in_seconds
-
-    def get_expire_time(self, path):
-        file_abspath = path + Storage.EXPIRE_EXT
-
-        if not exists(file_abspath):
-            return self.context.config.get("STORAGE_EXPIRATION_SECONDS", None), None
-
-        with open(file_abspath, "rb") as expire_file:
-            buffer = expire_file.read()
-            max_ages = buffer.decode().split(",")
-            if len(max_ages) == 2:
-                return int(max_ages[0]), int(max_ages[1])
-
-            return int(max_ages[0]), None
+        self.cache.remove(n_path)
