8bc50e429a91b33fc75aacb6b715ce75a0abec03
Daniel Czerwonk <czerwonk@mauve.de>
2023-05-10 12:16:24 +0200

    catch exceptions when statsd exporter is not resolvable

--- thumbor/metrics/statsd_metrics.py
+++ thumbor/metrics/statsd_metrics.py
@@ -10,23 +10,41 @@
 
 import statsd
 from thumbor.metrics import BaseMetrics
-
+from socket import gaierror
+from thumbor.utils import logger
 
 class Metrics(BaseMetrics):
+    @classmethod
+    def new_client(cls, config):
+        try:
+            return statsd.StatsClient(
+                config.STATSD_HOST, config.STATSD_PORT, config.STATSD_PREFIX
+            )
+        except gaierror:
+            logger.error("could not resolve statsd endpoint")
+            return None
+
     @classmethod
     def client(cls, config):
         """
         Cache statsd client so it doesn't do a DNS lookup
         over and over
         """
-        if not hasattr(cls, "_client"):
-            cls._client = statsd.StatsClient(
-                config.STATSD_HOST, config.STATSD_PORT, config.STATSD_PREFIX
-            )
+        if not hasattr(cls, "_client") or cls._client == None:
+            cls._client = Metrics.new_client(config)
         return cls._client
 
     def incr(self, metricname, value=1):
-        Metrics.client(self.config).incr(metricname, value)
+        cl = Metrics.client(self.config)
+        if cl == None:
+            return
+
+        cl.incr(metricname, value)
 
     def timing(self, metricname, value):
-        Metrics.client(self.config).timing(metricname, value)
+        cl = Metrics.client(self.config)
+        if cl == None:
+            return
+
+        cl.timing(metricname, value)
+
